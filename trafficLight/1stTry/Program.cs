58e750daffd883c83eacb7a78a8935c5fc966d1e
borislav1998 <borislav1998@users.noreply.github.com>
2019-02-27 14:26:40 +0100

    Stage 1

---
+++ trafficLight/1stTry/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1stTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("    Stop     ");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(3000);
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("  Get Ready  ");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(2000);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("    Go       ");
+
+         
+
+            Console.ReadLine();
+        }
+    }
+}
