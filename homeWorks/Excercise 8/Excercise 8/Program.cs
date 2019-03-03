61f94f835804be73b7e49e5af319b69965d8acf1
borislav1998 <borislav1998@users.noreply.github.com>
2019-03-03 22:55:06 +0100

    stage1

---
+++ homeWorks/Excercise 8/Excercise 8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 1,2 or 3");
+            int num;
+            bool input = Int32.TryParse(Console.ReadLine(), out num);
+            if (input = true && num <= 3)
+            {
+                    switch (num)
+                    {
+                        case 1:
+                            Console.WriteLine("You got a new car");
+                            break;
+                        case 2:
+                            Console.WriteLine("You got a new plane");
+                            break;
+                        case 3:
+                            Console.WriteLine("You got a new bike");
+                            break;
+                    };
+               
+
+            }
+            else
+            {
+                Console.WriteLine("You did something wrong!! Try again");
+            };
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homeWorks/Excericse 7/Excericse 7/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excericse_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number");
+            int firstInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            int secondInt = Convert.ToInt32(Console.ReadLine());
+
+            if (firstInt > secondInt)
+            {
+                Console.WriteLine("First number is bigger");
+                if (firstInt % 2 == 0)
+                {
+                    Console.WriteLine("The first number is even.");
+                }
+                else
+                {
+                    Console.WriteLine("The first number is odd.");
+                }
+
+            }
+            else if (secondInt > firstInt)
+            {
+                Console.WriteLine("Second number is bigger");
+                if (firstInt % 2 == 0)
+                {
+                    Console.WriteLine("The second number is even.");
+                }
+                else
+                {
+                    Console.WriteLine("The second number is odd.");
+                }
+            }
+
+            else if (firstInt == secondInt)
+            {
+                Console.WriteLine("Numbers are equal.");
+                if (firstInt % 2 == 0 && secondInt % 2 == 0)
+                {
+                    Console.WriteLine("The numbers are even.");
+                }
+                else
+                {
+                    Console.WriteLine("The numbers are odd.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Something happend!! Please try again.");
+            }
+            Console.ReadLine();
+          
+        }
+    }
+}
