1bf2936b26fa17d9239cdca07e7a12b5b35d657a
borislav1998 <borislav1998@users.noreply.github.com>
2019-03-03 22:58:11 +0100

    stage2

---
+++ homeWorks/HomeworkPart1/HomeworkPart1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the operant:");
+            char character = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+            int result;
+            switch (character)
+            {
+                case '+':
+                    result = firstNum + secondNum;
+                    Console.WriteLine("The result is: " + result);
+                    break;
+                case '-':
+                    result = firstNum - secondNum;
+                    Console.WriteLine("The result is: " + result);
+                    break;
+                case '*':
+                    result = firstNum * secondNum;
+                    Console.WriteLine("The result is: " + result);
+                    break;
+                case '/':
+                    result = firstNum / secondNum;
+                    Console.WriteLine("The result is: " + result);
+                    break;
+                default:
+                    Console.WriteLine("You entered wrong operant");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homeWorks/HomeworkPart2/HomeworkPart2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter amount in MKDenars");
+            double denars = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter USD,EUR or GBP");
+            string currency = Console.ReadLine();
+            double usd = 0.019;
+            double eur = 0.016;
+            double gbp = 0.014;
+            double result;
+            switch (currency)
+            {
+                case "USD":
+                    result = denars * usd;
+                    Console.WriteLine("The amount in USD is: " + result);
+                    break;
+                case "EUR":
+                    result = denars * eur;
+                    Console.WriteLine("The amount in EUR is: " + result);
+                    break; 
+                case "GBP":
+                    result = denars * gbp;
+                    Console.WriteLine("The amount in GBP is: " + result);
+                    break;
+                default:
+                    Console.WriteLine("You entered wrong currency");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
